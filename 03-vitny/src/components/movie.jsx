66ad1d5f6c9ebc2d7dd86b646878eeb9d88bac1f
jason jafari <jason.jafari.m@gmail.com>
2020-06-04 13:02:46 -0400

    vvv

--- 03-vitny/src/components/movie.jsx
+++ 03-vitny/src/components/movie.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Like from "./common/like"
 import { getMovies } from '../services/fakeMovieService'
 
 
@@ -11,6 +12,15 @@ class Movies extends Component {
         const movies = this.state.movies.filter(m => m._id !== movie._id)
         this.setState({ movies });
     };
+
+    handelLike = (movie)=>{
+        const movies = [...this.state.movies];
+        const index = movies.indexOf(movie);
+        movies[index] = {...movies[index]}
+        movies[index].liked = !movies[index].liked
+        this.setState({ movies });
+        // console.log("sss",movie)
+    }
 
     render() {
         const {length: count} = this.state.movies
@@ -27,6 +37,8 @@ class Movies extends Component {
                         <th>Genre</th>
                         <th>Stock</th>
                         <th>Rate</th>
+                        <th />
+                        <th />
                     </tr>
                 </thead>
 
@@ -37,6 +49,7 @@ class Movies extends Component {
                             <td>{movie.genre.name}</td>
                             <td>{movie.numberInStock}</td>
                             <td>{movie.dailyRentalRate}</td>
+                            <td><Like liked={movie.liked} onClick={()=>this.handelLike(movie)} /></td>
                             <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
                         </tr>
 
