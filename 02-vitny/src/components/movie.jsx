6c21cfd5cbf212f919b99b2257afc1ae7869be9f
jason jafari <jason.jafari.m@gmail.com>
2020-06-03 22:35:15 -0400

    good

--- 02-vitny/src/components/movie.jsx
+++ 02-vitny/src/components/movie.jsx
@@ -6,9 +6,20 @@ class Movies extends Component {
     state = {
         movies: getMovies()
     }
+
+    handleDelete = (movie) => {
+        const movies = this.state.movies.filter(m => m._id !== movie._id)
+        this.setState({ movies });
+    };
+
     render() {
-        console.log(this.state.movies)
+        const {length: count} = this.state.movies
+        if (count === 0) return <p>There are no Movies in the database</p>
+
+
         return (
+           <React.Fragment>
+                <p>Showing {count}  movies in the database</p>
             <table className="table">
                 <thead>
                     <tr>
@@ -26,12 +37,14 @@ class Movies extends Component {
                             <td>{movie.genre.name}</td>
                             <td>{movie.numberInStock}</td>
                             <td>{movie.dailyRentalRate}</td>
+                            <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
                         </tr>
 
                     ))}
 
                 </tbody>
             </table>
+           </React.Fragment>
         );
     }
 }
